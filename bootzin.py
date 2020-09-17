5922c2c26c516e860d283a0fbc5be76ac186d220
matheusfamaral <71414888+matheusfamaral@users.noreply.github.com>
2020-09-17 00:32:03 -0300

    Update bootzin.py

--- bootzin.py
+++ bootzin.py
@@ -18,7 +18,6 @@ for tweet in tweepy.Cursor(api.search, search).items(numero):
             print("nome do usuario: @" + tweet.user.screen_name)
             api.update_status(status="@" + tweet.user.screen_name + " o time do sexo", in_reply_to_status_id=tweet.id)
             print("tweet enviado corretamente")
-            time.sleep(30)
     except tweepy.TweepError as e:
         print(e.reason)
     except StopIteration:
